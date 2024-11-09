4905c29cb417911a2e54031d12a5c63faa29c9a6
Valeria Rojas <rojasvaleria820@gmail.com>
2024-11-08 19:18:52 -0500

    controlador usuario para el metodo registrar

---
+++ controllers/usuarios.php
@@ -0,0 +1,17 @@
+<?php
+
+if(isset($_POST)  && !empty($_POST)){
+    if(isset("register")){
+        $nombre = trim($_POST["name"]);
+        $apellido = trim($_POST["last_name"]);
+        $identificacion = trim($_POST["identificacion"]);
+        $correo = trim($_POST["email"]);
+        $usuario = trim($_POST["usuario"]);
+        $contraseña = trim($_POST["password"]);
+
+        include(../models/usuarios.php); 
+        $response = usuarios::registrar($nombre, $apellido, $identificacion, $correo, $usuario, $contraseña);
+    }
+}else{
+    
+}
